90800ba36d15c9d463e78def63f073e341b717a6
Ekaterina <alex40umov@gmail.com>
2022-11-11 16:59:52 +0300

    HomeWork07

---
+++ Test01/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Задайте двумерный массив размером m*n, заполненный случайными вещественными числами.
+
+int m = new Random().Next(3, 10);
+int n = new Random().Next(3, 10);
+Console.WriteLine($"Размер массива {m}*{n}");
+
+double[,] matrix = new double[m, n];
+matrix = FillArray(matrix);
+PrintArray(matrix);
+
+
+double[,] FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double randomNum = new Random().NextDouble();
+            matrix[i, j] = Math.Round(randomNum, 2);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Test03/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int row = new Random().Next(1, 10);
+int column = new Random().Next(1, 10);
+Console.WriteLine($"Размер массива {row}*{column}");
+
+int[,] matrix = new int[row, column];
+matrix = FillArray(matrix);
+PrintArray(matrix);
+AverageNum(matrix);
+
+int[,] FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageNum(int[,] matrix)
+{
+    Console.WriteLine("Среднее арифм-е каждого столбца:");
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        double result = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        result = sum / matrix.GetLength(0);
+        Console.Write($"c | ");
+    }
+}
